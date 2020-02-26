00623ced2e7a13155692a28c38c3fa31b4e75882
Shah Nawaz <shahshuvo@yahoo.com>
2020-02-27 01:54:51 +0600

    Completion of the first phase

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import {IncomeExpenses} from './components/IncomeExpenses'
 import {TransactionList} from './components/TransactionList'
 import {AddTransaction} from './components/AddTransaction'
 
+import { GlobalProvider } from './context/GlobalState'
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <Balance />
       <IncomeExpenses />
       <TransactionList />
       <AddTransaction />
-    </div>
+    </GlobalProvider>
   );
 }
 
